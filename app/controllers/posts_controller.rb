75574c6e9c6665342122092d0bf2770fc0a8bfee
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-11 17:20:22 +0900

    コントトーラー・ルーティング記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @post = Post.all
+    @posts = Post.order("created_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Pots.new(post_params)
+    if @post.save
+      render :show
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:item, :content, :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
   get "/" => "home#top"
   get "about" => "home#about"
+  resources :posts
 end
