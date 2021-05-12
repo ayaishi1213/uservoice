c99571c8fad5cd8c87ee4a00d99f07926c939299
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-12 14:45:57 +0900

    ユーザーコントローラー編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,33 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    # if params[:image]
+    #   @user.image_name = "#{@user.id}.jpg"
+    #   image = params[:image]
+    #   File.binwrite("public/#{@user.image_name}", image.read)
+    # end
+
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render("/users/edit")
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :age, :email, :profile)
+  end
+
 end
 
