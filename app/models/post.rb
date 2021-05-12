2f1adffc34a267620fd517a1e018f2e62a5e9d2e
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-12 23:29:51 +0900

    postモデル修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :users, through: :likes
 
-  def liked_by?(user)
-    likes.where(user_id: user.id).exists?
-  end
 end
