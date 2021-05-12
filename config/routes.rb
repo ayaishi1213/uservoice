28f953af6d5fb556a021664b8ea55ae12d5498e5
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-12 09:32:10 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get "/" => "home#top"
   get "about" => "home#about"
+
+  get "/users/:id" => "users#show"
+  get "/users/:id/edit" => "users#edit"
+  get "/users/:id/update" => "users#update"
+  
   resources :posts
 end
