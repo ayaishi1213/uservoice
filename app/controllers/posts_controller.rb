4aee9832f91d67975f4d364b33e5002d0dc74816
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-18 17:20:22 +0900

    showアクションのコメント機能の記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,8 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @likes_count = Like.where(post_id: @post.id).count
+    @comment = Comment.new
+    @comments = @post.comments
   end
 
   def edit
