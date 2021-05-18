8ab648dfcb42b08efeb1b71b444270daae934a60
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-18 19:40:37 +0900

    destroyアクション記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render :index
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:comment_content).merge(user_id: current_user.id, post_id: params[:post_id])
