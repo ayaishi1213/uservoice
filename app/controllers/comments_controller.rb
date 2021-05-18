274a25f8578ef1f6bfd85c3fcf774593d6451551
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-18 17:19:59 +0900

    createアクション記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:id])
+    @comment = @post.comments.build(comment_params)
+    
+    if @comment_content.save
+      render :index
+    end
+  end
 end
