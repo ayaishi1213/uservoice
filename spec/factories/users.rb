31127c7b4043e7fe9d1e2025e79536152a00390f
ayaishi1213 <shitianxuannai958@gmail.com>
2021-05-20 17:07:35 +0900

    user Factorybot設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
-  end
+    nickname {Facker::Name.name}
+    email {Facker::Internet.free_email}
+    password {'1a'+ Facker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    age {'11'}
 end
